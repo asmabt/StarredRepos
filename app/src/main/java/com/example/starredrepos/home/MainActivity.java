da3c9ebc22d5704a7fe9a3ee876bbac7f57e5df5
asmae boutkhil <asmae.boutkhil@adria-bt.com>
2019-12-29 04:24:23 +0100

    Add mainActivity's layout

--- app/src/main/java/com/example/starredrepos/home/MainActivity.java
+++ app/src/main/java/com/example/starredrepos/home/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.starredrepos;
+package com.example.starredrepos.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.starredrepos.R;
 
 public class MainActivity extends AppCompatActivity {
 
