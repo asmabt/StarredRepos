453a212165d619652b072c216d8cf9a7382ef090
asmae boutkhil <asmae.boutkhil@adria-bt.com>
2019-12-31 01:31:14 +0100

    Setting up Retrofit and creating method to retrieve the data

--- app/src/main/java/com/example/starredrepos/common/network/ApiInterface.java
+++ app/src/main/java/com/example/starredrepos/common/network/ApiInterface.java
@@ -1,11 +1,13 @@
 package com.example.starredrepos.common.network;
 
 import com.example.starredrepos.Utils.Constants;
+import com.example.starredrepos.models.StarredReposRS;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-public class ApiInterface {
+public interface ApiInterface {
 
-
+    @GET(Constants.STARRED_REPOS_URL)
+    Call<StarredReposRS> getStarredReposRS();
 }
--- app/src/main/java/com/example/starredrepos/common/network/ApiManager.java
+++ app/src/main/java/com/example/starredrepos/common/network/ApiManager.java
@@ -1,9 +1,15 @@
-package com.example.starredrepos.common;
+package com.example.starredrepos.common.network;
 
-import com.example.starredrepos.common.network.ApiInterface;
+import com.example.starredrepos.BuildConfig;
+import com.example.starredrepos.home.StarredReposApp;
+import com.example.starredrepos.models.StarredReposRS;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.readystatesoftware.chuck.ChuckInterceptor;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,6 +18,8 @@ public class ApiManager {
     private static ApiManager instance;
     private Gson gson;
     private ApiInterface apiInterface ;
+    private OkHttpClient client;
+
 
 
     private ApiManager(){
@@ -50,5 +58,8 @@ public class ApiManager {
         return gson;
     }
 
+    public Call<StarredReposRS> getStarredReposRS(){
+        return this.apiInterface.getStarredReposRS();
+    }
 
 }
