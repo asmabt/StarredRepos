65eb97cc77704f9577920eb3c151e84e7ada6a9a
asmae boutkhil <asmae.boutkhil@adria-bt.com>
2019-12-31 12:59:54 +0100

    Add Repo description, repo owner username and avatar

--- app/src/main/java/com/example/starredrepos/home/StarredReposAdapter.java
+++ app/src/main/java/com/example/starredrepos/home/StarredReposAdapter.java
@@ -6,11 +6,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.starredrepos.R;
 import com.example.starredrepos.models.Repos;
+import com.squareup.picasso.Picasso;
 
+import java.text.NumberFormat;
 import java.util.List;
 
 import butterknife.BindView;
@@ -37,7 +40,11 @@ public class StarredReposAdapter extends RecyclerView.Adapter<StarredReposAdapte
     @Override
     public void onBindViewHolder(@NonNull StarredReposAdapter.ViewHolder holder, int position) {
         holder.repoName.setText(repos.get(position).getName());
-
+        holder.repoDescription.setText(repos.get(position).getDescription());
+        holder.ownerUserName.setText(repos.get(position).getOwner().getLogin());
+        Picasso.with(context).load(repos.get(position).getOwner().getAvatarUrl()).into(holder.ownerAvatar);
+        NumberFormat numberFormat = NumberFormat.getNumberInstance();
+        holder.repoScore.setText(numberFormat.format(repos.get(position).getScore()));
     }
 
     @Override
@@ -53,6 +60,18 @@ public class StarredReposAdapter extends RecyclerView.Adapter<StarredReposAdapte
 
         @BindView(R.id.repo_name)
         TextView repoName;
+
+        @BindView(R.id.owner_username)
+        TextView ownerUserName;
+
+        @BindView(R.id.repo_description)
+        TextView repoDescription;
+
+        @BindView(R.id.owner_avatar)
+        ImageView ownerAvatar;
+
+        @BindView(R.id.repo_score)
+        TextView repoScore;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
