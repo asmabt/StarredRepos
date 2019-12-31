62faa34bf1410b5509245d0f5a1082f3cfc598d6
asmae boutkhil <asmae.boutkhil@adria-bt.com>
2019-12-31 02:33:58 +0100

    Add Feature : Listing most starred repos for the last 30 days

--- app/src/main/java/com/example/starredrepos/home/MainActivity.java
+++ app/src/main/java/com/example/starredrepos/home/MainActivity.java
@@ -2,14 +2,65 @@ package com.example.starredrepos.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.starredrepos.R;
+import com.example.starredrepos.models.Repos;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements homeView {
+
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    @Inject
+    homePresenter presenter;
+
+    private List<Repos> reposList = new ArrayList<>();
+
+    private StarredReposAdapter starredReposAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StarredReposApp.getInstance().setCurrentActivity(this);
+        ButterKnife.bind(this);
+        StarredReposApp.getInstance().getAppComponent().inject(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        starredReposAdapter = new StarredReposAdapter(this, reposList);
+        recyclerView.setAdapter(starredReposAdapter);
+
+        presenter.bind(this);
+        presenter.getStarredRepos();
+
+    }
+
+    @Override
+    public void showError(String message) {
+
+    }
+
+    @Override
+    public void getStarredReposList(List<Repos> starredReposList) {
+        reposList.addAll(starredReposList);
+        starredReposAdapter.setStarredReposList(reposList);
+    }
+
+    @Override
+    public void showError() {
+
     }
 }
